8baaaf827444a93054d41113a42e91f050e057d7
camillegil <gilcamille022@gmail.com>
2025-01-24 11:28:50 +0100

    Input example

--- Input_Example.py
+++ Input_Example.py
@@ -7,3 +7,6 @@ age2 = input(f"How old are you {name}? ")
 age2 = int(age2)
 print(f"{name}, you were born in {2024-age2}")
 
+age2 = input(f"How old are you {name}? ")
+age2 = int(age2)
+print(f"{name}, you were born in {2024-age2}")
