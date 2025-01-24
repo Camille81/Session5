084a16623bceb445e3436a606bc26bd9a9e165d8
camillegil <gilcamille022@gmail.com>
2025-01-24 11:42:58 +0100

    Input example

--- Input_Example.py
+++ Input_Example.py
@@ -8,5 +8,9 @@ age2 = int(age2)
 print(f"{name}, you were born in {2024-age2}")
 
 age2 = input(f"How old are you {name}? ")
-age2 = int(age2)
-print(f"{name}, you were born in {2024-age2}")
+try:
+    age2 = int(age2)
+    print(f"{name}, you were born in {2024-age2}")
+except:
+    print("please enter a valid value for age")
+    print("I can alos print this in case of error that I prevented")
